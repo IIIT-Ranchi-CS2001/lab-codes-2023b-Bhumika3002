a4eb6837ea54dec0caf473eaf842cd83624e62c3
Bhumika3002 <146648019+Bhumika3002@users.noreply.github.com>
2024-12-04 11:06:17 +0530

    Update ans.py

--- 2023UG3002/ans.py
+++ 2023UG3002/ans.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd 
 data=pd.read_csv("AQI_Data.csv")
-data.head(8) # to get starting 8 rows
-data.tail() #to get the last 5 rows
-data.info() # to get the data types and number of non null count 
-np.mean('AQI') # to get mean AQI 
-np.max('PM2.5') #to get max PM2.5
-np.min('PM10') # to get min PM10 
+print(data.head(8)) # to get starting 8 rows
+print(data.tail() )#to get the last 5 rows
+print(data.info() )# to get the data types and number of non null count 
+print(np.mean('AQI')) # to get mean AQI 
+print(np.max('PM2.5')) #to get max PM2.5
+print(np.min('PM10') )# to get min PM10 
 
 import pandas as pd
 data=pd.read_csv('AQI_Data.csv')
-group=data.groupby['City'].avg('AQI').max('PM2.5').min('PM10')
+print(group=data.groupby['City'].avg('AQI').max('PM2.5').min('PM10'))
